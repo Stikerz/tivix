197c1e34bc470fd04ba74965076ff398fb03b80f
Stikerz <abiola_saraki@live.co.uk>
2019-12-01 03:17:05 +0000

    create api app and user models

---
+++ tivix/api/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=150, blank=False, null=False)
+    last_name = models.CharField(max_length=150, blank=False, null=False)
+    age = models.DateField(null=False)
+    year = models.IntegerField(null=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Teacher(AbstractUser):
+    students = models.ManyToManyField(Student, blank=True)
+    first_name = models.CharField(max_length=150, blank=False, null=False)
+    last_name = models.CharField(max_length=150, blank=False, null=False)
+    email = models.EmailField(unique=True, blank=False, null=False)
+
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
