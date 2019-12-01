0f0afacf6955f1d701dd8a422c4e7e0894b24d98
Stikerz <abiola_saraki@live.co.uk>
2019-12-01 08:56:34 +0000

    add teacher serializer

--- tivix/api/serializers.py
+++ tivix/api/serializers.py
@@ -15,6 +15,17 @@ class TeacherSerializer(ModelSerializer):
         model = Teacher
         fields = ['username', 'first_name', 'last_name', 'email', 'students']
 
+    def update(self, instance, validated_data):
+        # TODO : User Permission check - only user can edit own instance info
+        instance.username = validated_data['username']
+        instance.first_name = validated_data['first_name']
+        instance.last_name = validated_data['last_name']
+        instance.email = validated_data['email']
+        instance.students.add(*[Student.objects.get(**student) for student in
+                                validated_data['students']])
+
+        return instance
+
 
 class StartStudentSerializer(ModelSerializer):
     student = StudentSerializer(many=True)
