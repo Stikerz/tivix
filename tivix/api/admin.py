3f55b76e0011a1a8be53ab7b50ddb9b2b323c2d2
Stikerz <abiola_saraki@live.co.uk>
2019-12-01 06:34:01 +0000

    update admin site

--- tivix/api/admin.py
+++ tivix/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Teacher, Student
+from .models import Teacher, Student, StarStudent
 # Register your models here.
 
 admin.site.register(Student)
 admin.site.register(Teacher)
+admin.site.register(StarStudent)
 
